d3e6abdc0a2b7a54e49631fec86ea25f32df79d9
Edwar Frank Pons Bessio <edwar.servidor@gmail.com>
2022-08-12 22:37:52 -0300

    Ejercicio 2 del Open Bootcamp

---
+++ index.js
@@ -0,0 +1,24 @@
+const mio = [
+ "Edwar Frank Pons Bessio",
+ 42,
+ true,
+ "10/02/1980",
+ {
+  titulo: "Vampiro, Edad Oscura",
+  autor: "David A Hill Jr",
+  fecha: "01/01/1242",
+  url: "https://www.nosolorol.com/es/",
+ },
+];
+
+console.log("Mis Datos Personales");
+console.log("  Nombre y Apellido:", mio[0]);
+console.log("  Edad:", mio[1]);
+console.log("  Soy Desarrollador:", mio[2]);
+console.log("  Fecha de Nacimiento:", mio[3]);
+console.log("\nMi libro favorito");
+console.log("  Título:", mio[4].titulo);
+console.log("  Autor:", mio[4].autor);
+console.log("  Fecha:", mio[4].fecha);
+console.log("  URL:", mio[4].url);
+console.log("\n");
